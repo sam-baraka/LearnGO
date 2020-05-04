9b78ce684fab6be987e12755a8d15085c09ef1a2
Samuel Baraka <sababuvercetti@gmail.com>
2020-05-05 00:38:15 +0300

    First part

---
+++ JSON/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Book struct {
+	Title  string `json:"title"`
+	Author Author `json:"author"`
+}
+
+type Author struct {
+	Name      string `json:"name"`
+	Age       int    `json:"age"`
+	Developer bool   `json:"is_developer"`
+}
+
+func main() {
+
+	author := Author{Name: "Samuel Baraka", Age: 22, Developer: true}
+
+	book := Book{Title: "Learning JSON in GO", Author: author}
+	fmt.Printf("%+v\n", book)
+	byteArray, err := json.MarshalIndent(book, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(byteArray))
+}
