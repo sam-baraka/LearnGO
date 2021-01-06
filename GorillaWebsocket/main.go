b3be5d91899e837ed96b90facc6ba69ab03173dc
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-01-06 23:47:57 +0300

    Finished gorilla websocket tutorial

--- GorillaWebsocket/main.go
+++ GorillaWebsocket/main.go
@@ -4,14 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Home Page")
 }
 
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Websocket Endpoint")
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("CLient connected")
+
+	reader(ws)
+
 }
 
 func setupRoutes() {
