c2d00464cd5627551f45edfea5e5284c7b0f025d
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-01-06 23:07:21 +0300

    Fixed errors

--- Websockets/main.go
+++ Websockets/main.go
@@ -1,3 +1,26 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func main() {
+	fmt.Println("Hello Sam")
+
+	server := socketio.NewServer(nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		log.Println("New Connection")
+		return nil
+	})
+
+	http.Handle("/socket.io/", server)
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
